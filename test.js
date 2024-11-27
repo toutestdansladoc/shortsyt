a8846d7834ba61b9a57aa0f23cf1dc3eb2a0af89
toutestdansladoc <toutestdansladoc@gmail.com>
2024-11-27 16:46:25 +0100

    ytdl

--- test.js
+++ test.js
@@ -5,23 +5,11 @@ const youtubesearchapi = require("youtube-search-api");
 let videosDl = []
 let ytres = youtubesearchapi.GetListByKeyword("funny pet", true, 1, [{ type: "video" }])
     .then((res) => {
-        console.log("Page1");
         res.items.forEach(element => {
             videosDl.push(element.id)
 
         });
         for (idVideo of videosDl) {
-            
-            ytdl("http://www.youtube.com/watch?v="+idVideo).pipe(require("fs").createWriteStream(idVideo));
+            ytdl("http://www.youtube.com/watch?v=" + idVideo).pipe(require("fs").createWriteStream(idVideo));
         }
-
     })
-
-
-// let videos=[{name:"video1",url:"http://www.youtube.com/watch?v=aqz-KE-bpKQ"},{name:"video2",url:"http://www.youtube.com/watch?v=aqz-KE-bpKQ"}]
-// // Download a video
-// for(video of videos){
-//     console.log(video.name)
-//     ytdl(video.url).pipe(require("fs").createWriteStream(video.name));
-// }
-
